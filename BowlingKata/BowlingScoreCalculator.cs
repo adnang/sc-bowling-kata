d84a6aa1d8239b3df86251d8e7eb10fab667bd5b
Adnan Gazi <adnan.gazi@asos.com>
2016-12-16 13:23:31 +0000

    refactor - consolidate second pin score logic

--- BowlingKata/BowlingScoreCalculator.cs
+++ BowlingKata/BowlingScoreCalculator.cs
@@ -7,16 +7,15 @@
             if (game.Equals("--|--|--|--|--|--|--|--|--|--||--"))
                 return 0;
 
-            if (game.Equals("-1|--|--|--|--|--|--|--|--|--||--"))
-                return 1;
-
-            if (game.Equals("-2|--|--|--|--|--|--|--|--|--||--"))
-                return 2;
-
-            if (game.Equals("-3|--|--|--|--|--|--|--|--|--||--"))
-                return 3;
+            if (game.StartsWith("-"))
+                return SecondTurnScore(game);
 
             return game.FirstPinScore();
+        }
+
+        private static int SecondTurnScore(string game)
+        {
+            return int.Parse(game.Substring(1, 1));
         }
     }
 
