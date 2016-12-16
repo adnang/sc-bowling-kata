517fb37c124090dc6e0d17b488c1a9f90444a77a
Adnan Gazi <adnan.gazi@asos.com>
2016-12-16 10:03:12 +0000

    returns 0 when calculating score given all misses

---
+++ BowlingKata.Tests/BowlingScoreCalculatorShould.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace BowlingKata.Tests
+{
+    [TestFixture]
+    public class BowlingScoreCalculatorShould
+    {
+        [Test]
+        public void Return0_WhenCalculatingScore_GivenAllMisses()
+        {
+            var score = new BowlingScoreCalculator().CalculateScore("--|--|--|--|--|--|--|--|--|--||--");
+            score.Should().Be(0);
+        }
+    }
+
+    public class BowlingScoreCalculator
+    {
+        public int CalculateScore(string game)
+        {
+            return 0;
+        }
+    }
+}
