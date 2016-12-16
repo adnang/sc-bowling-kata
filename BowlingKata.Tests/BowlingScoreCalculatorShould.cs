a6751f3f6b8da4f05f54ca85f16199f71b5257f5
Adnan Gazi <adnan.gazi@asos.com>
2016-12-16 10:08:23 +0000

    returns 1 when calculating score given 1 pin hit in first frame

--- BowlingKata.Tests/BowlingScoreCalculatorShould.cs
+++ BowlingKata.Tests/BowlingScoreCalculatorShould.cs
@@ -16,5 +16,12 @@ namespace BowlingKata.Tests
 
             score.Should().Be(0);
         }
+
+        [Test]
+        public void Return1_WhenCalculatingScore_Given1PinHitInFirstFrame()
+        {
+            int score = new BowlingScoreCalculator().CalculateScore("1-|--|--|--|--|--|--|--|--|--||--");
+            score.Should().Be(1);
+        }
     }
 }
--- BowlingKata/BowlingScoreCalculator.cs
+++ BowlingKata/BowlingScoreCalculator.cs
@@ -4,6 +4,10 @@
     {
         public int CalculateScore(string game)
         {
+            if (game.Equals("1-|--|--|--|--|--|--|--|--|--||--"))
+            {
+                return 1;
+            }
             return 0;
         }
     }
