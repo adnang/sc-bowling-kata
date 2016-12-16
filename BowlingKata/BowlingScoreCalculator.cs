04105f8c83da623b516845e0762d2a202377d539
Adnan Gazi <adnan.gazi@asos.com>
2016-12-16 12:20:25 +0000

    refactor - consolidate first pin logic

--- BowlingKata/BowlingScoreCalculator.cs
+++ BowlingKata/BowlingScoreCalculator.cs
@@ -4,22 +4,10 @@
     {
         public int CalculateScore(string game)
         {
-            if (game.Equals("1-|--|--|--|--|--|--|--|--|--||--"))
-            {
-                return 1;
-            }
+            if (game.Equals("--|--|--|--|--|--|--|--|--|--||--"))
+                return 0;
 
-            if (game.Equals("2-|--|--|--|--|--|--|--|--|--||--"))
-            {
-                return 2;
-            }
-
-            if (game.Equals("3-|--|--|--|--|--|--|--|--|--||--"))
-            {
-                return 3;
-            }
-
-            return 0;
+            return int.Parse(game.Substring(0, 1));
         }
     }
 }
